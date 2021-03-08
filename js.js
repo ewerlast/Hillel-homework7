5e9a8d4bf0239930074aa30342d2ada5c7ed51fc
ewerlast1984 <ewerlast1984@gmail.com>
2021-03-08 09:54:49 +0200

    homework7/2

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+console.log("Task 1")
+
 const a = {
     mame: "Slavik",
     age: 37,
@@ -5,57 +7,78 @@ const a = {
     sun: {
         name: "gleb",
         age: 6,
-        famaly: ["father", "mother", "wife", "sun"],
+        famaly: ["father", "mather", "wife", "sun",],
     },
     method: function test() {
         return this.test;
-    }
+    },
+
 
 }
 
 
-let b = {};
-let chackObject = function (obj2, obj1) {
-    for (let key in obj1) {
-        if (typeof (obj1[key]) === "object") {
-            obj2[key] = chackObject({}, obj1[key])
+
+
+let chackObject = function (obj1) {
+    if (Array.isArray(obj1)) {
+        function chechArrey(obj1) {
+            let copyArrey = [];
+            for (let i = 0; i < obj1.length; i++) {
+                copiArrey[i] = obj1[i];
+            }
+            return copyArrey;
         }
-        if (Array.isArray(obj1[key])) {
-            obj2[key] = [];
-            for (let i = 0; i < obj1[key].length; i++) {
-                obj2[key][i] = obj1[key][i];
+    }
+    if (obj1 === null) {
+        return obj1;
+    }
+    if (obj1 !== null || (typeof (obj1) !== "object")) {
+        let notObject = obj1;
+        return notObject;
+
+    } else {
+
+        let obj2 = {};
+        for (let key in obj1) {
+            if (typeof (obj1[key]) === "object") {
+                obj2[key] = chackObject({}, obj1[key])
+            }
+
+            if (Array.isArray(obj1[key])) {
+                chechArrey(obj1[key]);
+            } else {
+                obj2[key] = obj1[key];
             }
         }
-
-        else {
-            obj2[key] = obj1[key];
-        }
+        return obj2;
     }
-    return obj2;
 }
-chackObject(b, a);
-console.log(b);
-console.log(a.method());
-console.log(b.method());
 
+let c = null;
+let d = 1;
+let arr = [1, 2, 3, 4];
+b = chackObject(c);
+console.log(b);
+b = chackObject(d);
+console.log(b);
+b = chackObject(arr);
+console.log(b);
+b = chackObject(a);
+console.log(b);
+
+
+console.log("Task 2")
 
 function sum() {
-    let count = 0;
     let b = 0;
     return function (a) {
-        count++;
-        if (count === 0) {
-            return b = a;
-
-        } else {
-            b = b + a
-            console.log(b);
-            return b;
-        }
+        b = b+a;
+        console.log(b);
+        return b
     }
 }
 let reslt = sum();
-reslt(2);
+reslt(6);
 reslt(4);
 reslt(15);
 reslt(25);
